c2ada5da483f2773dce49e5fa87957dea033cbe0
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-03-09 00:46:15 +0100

    Initial commit

---
+++ src/main/java/org/projects/statx/StatXApplication.java
@@ -0,0 +1,13 @@
+package org.projects.statx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/projects/statx/StatXApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projects.statx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
