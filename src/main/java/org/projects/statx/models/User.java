935086d1a730dfdcb485305d7b23be3d13652783
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-03-12 21:31:44 +0100

    Little changes

---
+++ src/main/java/org/projects/statx/models/User.java
@@ -0,0 +1,18 @@
+package org.projects.statx.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+}
