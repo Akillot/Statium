8ed58043c96e1510bf986ca20afc20a283e9dc2b
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-03-16 23:24:21 +0100

    Little changes

---
+++ src/main/java/org/projects/statx/entity/SocialLink.java
@@ -0,0 +1,4 @@
+package org.projects.statx.entity;
+
+public class SocialLink {
+}
---
+++ src/main/java/org/projects/statx/entity/Stats.java
@@ -0,0 +1,29 @@
+package org.projects.statx.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "stats")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Stats {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stat_id")
+    private Long stat_id;
+
+    @ManyToOne
+    @JoinColumn(name = "link_id", nullable = false)
+    private SocialLink socialLink;
+
+    @Column(name = "followers", columnDefinition = "INT DEFAULT 0")
+    private int followers = 0;
+
+
+}
