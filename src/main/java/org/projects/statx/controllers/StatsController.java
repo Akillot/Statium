678f7c6e0621612cee9a74b2472ef081e75882b8
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-03-24 22:33:09 +0100

    Stats controller has been created

---
+++ src/main/java/org/projects/statx/controllers/StatsController.java
@@ -0,0 +1,40 @@
+package org.projects.statx.controllers;
+
+import org.projects.statx.entity.Stats;
+import org.projects.statx.repository.StatsRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/stats")
+public class StatsController {
+    private final StatsRepository statsRepository;
+
+    public StatsController(StatsRepository statsRepository) {
+        this.statsRepository = statsRepository;
+    }
+
+    @GetMapping
+    public List<Stats> getStats() {
+        return statsRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Stats> getStatsById(@PathVariable Long id) {
+        Optional<Stats> stats = statsRepository.findById(id);
+        return stats.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Stats createStats(@RequestBody Stats stats) {
+        return statsRepository.save(stats);
+    }
+
+    @GetMapping("/social-link/{linkId}")
+    public List<Stats> getStatsBySocialLink(@PathVariable Long linkId) {
+        return statsRepository.findBySocialLinkId(linkId);
+    }
+}
