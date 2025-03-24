8f8b4f1d599203f5b9295ac4cd5ef8039d3f7677
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-03-24 22:10:54 +0100

    User controller has been created

---
+++ src/main/java/org/projects/statx/controllers/UserController.java
@@ -0,0 +1,40 @@
+package org.projects.statx.controllers;
+
+import org.projects.statx.entity.User;
+import org.projects.statx.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api/users")
+public class UserController {
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    //GET /api/users - get all users
+    //GET /api/users/{id} - get user by ID
+    //POST /api/users - create user
+}
