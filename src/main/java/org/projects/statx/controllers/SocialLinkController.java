07a8914fed4a9d499f8e9758281b6a8abb90124e
Nick Zozulia <156462532+Akillot@users.noreply.github.com>
2025-03-26 23:06:33 +0100

    social link controller has been updated

---
+++ src/main/java/org/projects/statx/controllers/SocialLinkController.java
@@ -0,0 +1,43 @@
+package org.projects.statx.controllers;
+
+import org.projects.statx.entity.SocialLink;
+import org.projects.statx.repository.SocialLinkRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api/social-links")
+public class SocialLinkController {
+
+    private final SocialLinkRepository socialLinkRepository;
+
+    public SocialLinkController(SocialLinkRepository socialLinkRepository) {
+        this.socialLinkRepository = socialLinkRepository;
+    }
+
+    @GetMapping
+    public List<SocialLink> getSocialLinks() {
+        return socialLinkRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SocialLink> getLinkById(@PathVariable Long id) {
+        Optional<SocialLink> link = socialLinkRepository.findById(id);
+        return link.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public SocialLink createLink(@RequestBody SocialLink socialLink) {
+        return socialLinkRepository.save(socialLink);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<SocialLink>> getLinksByUserId(@PathVariable Long userId) {
+        List<SocialLink> links = socialLinkRepository.findByUserId(userId);
+        return ResponseEntity.ok(links);
+    }
+}
